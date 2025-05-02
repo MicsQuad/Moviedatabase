2ab7500432980cea678fec47af69c0786a2652e8
Marta Moroni <marta.moroni0203@stud.hkr.se>
2025-05-02 13:49:46 +0200

    clean client folder and add frontend placeholders

---
+++ client/src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MovieCard() {
+  return (
+    <div>MovieCard placeholder</div>
+  );
+}
+
+export default MovieCard;
---
+++ client/src/components/MovieList/MovieList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MovieCard from '../MovieCard/MovieCard';
+
+function MovieList() {
+    return (
+      <div> MoiveList placeholder</div>
+    );
+  }
+  
+  export default MovieList;
