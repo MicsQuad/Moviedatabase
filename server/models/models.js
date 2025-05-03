43319d33ec810d080b1c6834bb64fcc49666d5ee
Chathuli-MJ <chathuli_madunima.jayasinghe0041@stud.hkr.se>
2025-05-04 00:35:17 +0200

    update model for watchstatus collection

--- server/models/models.js
+++ server/models/models.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const memberSchema = require('./Member.js');
 const movieSchema = require('./Movie.js');
 const favouriteSchema = require('./Favourite.js');
+const watchStatusSchema = require('./WatchStatus.js');
 
 dotenv.config();
 
@@ -13,9 +14,11 @@ const movieConnection = mongoose.createConnection(process.env.MONGO_URI_MOVIE,{}
 const Member = userConnection.model('Member', memberSchema);
 const Movie = movieConnection.model('Movie', movieSchema);
 const Favourite = movieConnection.model('Favourite', favouriteSchema);
+const WatchStatus = movieConnection.model('WatchStatus', watchStatusSchema);
 
 module.exports = {
     Member,
     Movie,
     Favourite,
+    WatchStatus,
 };
