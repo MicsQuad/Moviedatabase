29043fb5752e21426da15b13bbaa26b574a49ef4
MicsQuad <jegkorszak4@gmail.com>
2025-05-14 11:09:34 +0200

    Added JWT checking for extra security. Changed README accordingly.

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const registerRoutes = require('./routes/register');
 const favouriteRoutes = require('./routes/favourite');
 const watchlistRoutes = require('./routes/watchlist');
 
+const path = require('path');
+
 dotenv.config();
 
 const app = express();
@@ -22,9 +24,10 @@ app.use('/api', loginRoutes);
 app.use('/api', registerRoutes);
 app.use('/api', favouriteRoutes);
 app.use('/api', watchlistRoutes);
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Movie Database API!');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 })
 
 const PORT = process.env.PORT;
