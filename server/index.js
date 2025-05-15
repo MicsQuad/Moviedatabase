73e139fcc5f75a549aa5d2ef672e6d6770701b2a
MicsQuad <jegkorszak4@gmail.com>
2025-05-15 21:21:52 +0200

    Removed unneccessary new line from index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: 'moviedatabase-5ozm.vercel.app\n',
+    origin: 'moviedatabase-5ozm.vercel.app',
     credentials: true,
 }));
 app.use(express.json());
